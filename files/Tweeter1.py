bd249d16bc933f9a6ea990691365498f72dfa662
mecolmg <mecolmg@vt.edu>
2015-03-08 23:56:59 -0400

    Deleted Files, Updated Webpage, Updated Script

--- files/Tweeter1.py
+++ files/Tweeter1.py
@@ -1,18 +1,29 @@
 import csv
+import plotly.plotly as py
+from plotly.graph_objs import *
 from collections import defaultdict
+import plotly.tools as tls
+tls.set_credentials_file(username='mecolmg', api_key='tub4jew75t')
 
 #Contains Tweet Data
 class Tweet:
     def __init__(self, data):
-        self.time,self.tweetid,self.text,self.rt,self.geo,self.placetag,self.favs,self.usr,self.usrloc,self.usrid,self.timezone,self.usrfollow,self.usrstats,self.usrfriends,self.usrhandle,self.hashtags,self.mentions = data
+        self.time, self.tweetid, self.text, self.rt, \
+        self.geo, self.placetag, self.favs, self.usr, \
+        self.usrloc, self.usrid, self.timezone, self.usrfollow, \
+        self.usrstats, self.usrfriends, self.usrhandle, \
+        self.hashtags, self.mentions = data
 
-#Imports data to the variable 'tweets'
-with open('oscar_tweets.csv', 'rb') as theFile:
-    theFile = csv.reader(theFile, delimiter=',',quotechar='"')
-    tweets = [Tweet(data) for data in theFile][1:]
-with open('states.csv', 'rb') as theFile:
-    theFile = csv.reader(theFile, delimiter=',',quotechar='"')
-    states = [data for data in theFile]
+#Imports data as Tweet objects to the variable 'tweets'
+with open('oscar_tweets.csv', 'rb') as File:
+    File = csv.reader(File, delimiter=',',quotechar='"')
+    tweets = [Tweet(data) for data in File][1:]
+    
+#Imports a list of states from a CSV file to the variable 'states'
+#For use in location() method
+with open('states.csv', 'rb') as File:
+    File = csv.reader(File, delimiter=',',quotechar='"')
+    states = [data for data in File]
 
 #Determines Most Tweeted Nominees
 def popularity():
@@ -24,11 +35,25 @@ def popularity():
             if text.count(nominee.lower().strip('the ')) != 0:
                 count[nominee] += 1
     top = sorted(count.items(),key=lambda x:x[1], reverse=True)
+    data = Data([Bar(x=[data[0] for data in top], y=[data[1] for data in top],
+                     marker=Marker(color='#b09953'))])
+    layout = Layout(
+        title='Tweets about Best Picture Nominees',
+        font=Font(
+            family='"Open sans", verdana, arial, sans-serif',
+            size=17,
+            color='#000'
+        ),
+        yaxis=YAxis(title='Number of Tweets')
+        )
+    fig = Figure(data=data,layout=layout)
+    plot = py.plot(fig)
     count = 1
     print("The Most Tweeted About Best Picture Nominees:")
     for t in top:
         print("\t"+str(count)+": "+t[0])
         count += 1
+    return top
 
 #Determines when Birdman (the winner) was most tweeted about
 def winner():
@@ -55,9 +80,4 @@ def location():
     for i in range(10):
         print("\t" + str(i+1)+": "+times[i][0])
 
-#Runs through previous functions to print results
-popularity()
-print
 winner()
-print
-location()
